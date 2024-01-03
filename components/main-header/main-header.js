071cd6c2b0fa1f46489a9206bea9d189eee3fc4c
Tomek <tomek.bielicki@gmail.com>
2024-01-03 17:55:56 +0100

    relocation header component

---
+++ components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+import MainHeaderBackground from "./main-header-background";
+import Image from "next/image";
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate withg food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
