ed02e0d28457be01145d5e3935f7a6a613ab9e6c
Tomek <tomek.bielicki@gmail.com>
2024-01-20 22:10:44 +0100

    3s aws added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  mages: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tomekbielicki-nextjs-demo-users-image.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
